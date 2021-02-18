9b5c3896ee06c86649999d29fd3b9bb0c2746ff1
Adrian Gramada <adrian.gramada@gmail.com>
2021-02-18 20:44:32 +0000

    Fixes on the base node commands.

--- provision/scripts/node.sh
+++ provision/scripts/node.sh
@@ -4,9 +4,6 @@
 echo 'sudo yum -y install wget net-tools mlocate'
 sudo yum -y install wget net-tools mlocate
 
-echo 'wget https://winscp.net/download/WinSCP-5.17.10-Setup.exe'
-sudo wget https://winscp.net/download/WinSCP-5.17.10-Setup.exe
-
 sudo wget https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
 #echo 'sudo mv /opt/vagrant/RPM-GPG-KEY-EPEL-7 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7'
 sudo mv RPM-GPG-KEY-EPEL-7 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
@@ -15,7 +12,7 @@ echo 'sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7'
 sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
 sudo rpm -qa gpg*
 
-sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo wget https://github.com/graadi/development-environment/blob/main/provision/files/epel/epel-release-latest-7.noarch.rpm
 #echo 'sudo rpm -ivh /opt/vagrant/epel-release-latest-7.noarch.rpm'
 sudo rpm -ivh epel-release-latest-7.noarch.rpm
 
